4c5491eb03f45f503b0532f709ee50d0e8fd8d47
Rondell <rondelllit16@gmail.com>
2025-05-07 00:38:18 +0800

    main python file

--- Lab3.py
+++ Lab3.py
@@ -3,7 +3,6 @@ print("Lab 3 - Software Unit Testing with PyTest")
 SORT_ASCENDING = 0
 SORT_DESCENDING = 1
 
-
 def bubble_sort(arr, sorting_order):
 
     # Copy input list to results list
@@ -12,29 +11,35 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+    if n == 0: return 0
 
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
+    if n >=10 : return 1 
+    
+    for num in arr_result:
+        if type(num) is not int: return 2
+    
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            if sorting_order == SORT_ASCENDING:
+                if arr_result[j] > arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+            elif sorting_order == SORT_DESCENDING:
+                if arr_result[j] < arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
-                else:
-                    # Return an empty array
-                    arr_result = []
-    else:
-        arr_result = -1
+            else:
+                # Return an empty array
+                arr_result = []
+
+    
 
     return arr_result
 
